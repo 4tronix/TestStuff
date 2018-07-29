c29f16df3f03d5f5019c3a0a5db88109f77bbec9
4tronix <Sales@4tronix.co.uk>
2018-07-29 17:15:43 +0100

    95

--- main.ts
+++ main.ts
@@ -94,7 +94,7 @@ namespace cubebit {
      * @param pin Micro:Bit pin to connect to Cube:Bit
      * @param side number of pixels on each side
      */
-    //% blockId="cubebit_create" block="create 94 Cube:Bit on %pin| with side %side"
+    //% blockId="cubebit_create" block="create 95 Cube:Bit on %pin| with side %side"
     //% weight=98
     //% side.min=3 side.max=8
     export function create(pin: DigitalPin, side: number): void
@@ -174,7 +174,7 @@ namespace cubebit {
             case CBBase.YX: z = cubeSide - z - 1; break;
             case CBBase.XZ: t = cubeSide - z - 1; z = y; y = t; break;
             case CBBase.ZX: t = cubeSide - y - 1; y = z; z = t; break;
-            case CBBase.YZ: t = cubeSide - y - 1; y = cubeSide - z - 1; z = x; x = t; break;
+            case CBBase.YZ: t = cubeSide - y - 1; y = z; z = x; x = t; break;
             case CBBase.ZY: t = cubeSide - z - 1; z = cubeSide - x - 1; x = y; y = t; break;
             default: z = z;
         }
