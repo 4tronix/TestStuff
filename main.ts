e359d160c290b913eb1f53a33c25903c75cc414d
4tronix <Sales@4tronix.co.uk>
2018-11-22 16:21:43 +0000

    s12

--- main.ts
+++ main.ts
@@ -75,7 +75,7 @@ namespace robobit {
       *
       * @param model Model of Robobit buggy. Mk1, Mk2, or Mk3
       */
-    //% blockId="robobit_model" block="select Robobit model %model"
+    //% blockId="robobit_model" block="select 12Robobit model %model"
     //% weight=110
     export function select_model(model: RBModel): void {
         _model = model;
