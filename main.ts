2e11e59ef1cac08ebb4759eef75fac1dc166b065
4tronix <Sales@4tronix.co.uk>
2018-11-23 17:44:29 +0000

    s21

--- main.ts
+++ main.ts
@@ -65,16 +65,39 @@ enum RBPingUnit {
 namespace Animoid {
 
     let _model: RBModel;
+    let PCA = 0x40;
+    let initI2C = false;
 
     /**
       * Select Model of Robobit (Determines Pin usage)
       *
       * @param model Model of Robobit buggy. Mk1, Mk2, or Mk3
       */
-    //% blockId="robobit_model" block="select 20Robobit model %model"
+    //% blockId="robobit_model" block="select 21Robobit model %model"
     //% weight=110
     export function select_model(model: RBModel): void {
         _model = model;
+    }
+
+    // Helper functions
+    function initPCA(): void
+    {
+/*
+        let i2cData = pins.createBuffer(2);
+        initI2C = true;
+
+        i2cData[0] = 0;		// Mode 1 register
+        i2cData[1] = 0x10;	// put to sleep
+        pins.i2cWriteBuffer(PCA, i2cData, false);
+
+        i2cData[0] = 0xFE;	// Prescale register
+        i2cData[1] = 101;	// set to 60 Hz
+        pins.i2cWriteBuffer(PCA, i2cData, false);
+
+        i2cData[0] = 0;		// Mode 1 register
+        i2cData[1] = 0x81;	// Wake up
+        pins.i2cWriteBuffer(PCA, i2cData, false);
+*/
     }
 
 }
