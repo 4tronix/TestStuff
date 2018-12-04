412eff7901026ba70f62c312d34f715f68ac40c8
4tronix <Sales@4tronix.co.uk>
2018-12-04 14:42:26 +0000

    12

--- main.ts
+++ main.ts
@@ -473,7 +473,7 @@ namespace robobit {
       * Start Scanner
       * @param delay time in ms between scan steps, eg: 100,50,200,500
       */
-    //% blockId="rb_startScanner" block="start 11 scanner speed %delay|(ms)"
+    //% blockId="rb_startScanner" block="start 12 scanner speed %delay"
     //% subcategory=LedBar
     //% group=LedBar
     //% delay.min=1 delay.max=10000
