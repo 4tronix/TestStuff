db057a9b0452e597c28a082c9569539032db2bf5
4tronix <Sales@4tronix.co.uk>
2018-12-10 14:05:04 +0000

    08

--- main.ts
+++ main.ts
@@ -26,8 +26,19 @@ enum BCJoystick {
 }
 
 /**
- * Pre-Defined LED colours
- */
+  * Update mode for LEDs
+  * setting to Manual requires "show LED changes" blocks
+  * setting to Auto will update the LEDs everytime they change
+  */
+enum BCMode
+{
+    Manual,
+    Auto
+}
+
+/**
+  * Pre-Defined LED colours
+  */
 enum BCColors
 {
     //% block=red
@@ -87,6 +98,7 @@ enum BCEvents {
 namespace bitcommander
 {
     let neoStrip: neopixel.Strip;
+    let _updateMode = BCMode.Auto;
 
 // Inputs. Buttons, Dial and Joystick
 
@@ -100,7 +112,7 @@ namespace bitcommander
       * Registers event code
       */
     //% weight=90
-    //% blockId=bc_onevent block="on 07 button %button|%event"
+    //% blockId=bc_onevent block="on 08 button %button|%event"
     //% subcategory=Inputs
     //% group=Inputs
     export function onEvent(button: BCPins, event: BCEvents, handler: Action)
@@ -199,6 +211,8 @@ namespace bitcommander
     export function neoSetColor(rgb: number)
     {
         neo().showColor(rgb);
+        if (_updateMode == BCMode.Auto)
+            neo().show();
     }
 
     /**
@@ -214,11 +228,11 @@ namespace bitcommander
     }
 
     /**
-     * Set LED to a given color (range 0-255 for r, g, b).
-     *
-     * @param ledId position of the LED (0 to 5)
-     * @param rgb RGB color of the LED
-     */
+      * Set LED to a given color (range 0-255 for r, g, b).
+      *
+      * @param ledId position of the LED (0 to 5)
+      * @param rgb RGB color of the LED
+      */
     //% blockId="bitcommander_neo_set_pixel_color" block="set LED at %ledId|to %rgb=bc_colours"
     //% weight=85
     //% subcategory=Leds
@@ -241,8 +255,8 @@ namespace bitcommander
     }
 
     /**
-     * Rotate LEDs forward.
-     */
+      * Rotate LEDs forward.
+      */
     //% blockId="bitcommander_neo_rotate" block="rotate LEDs"
     //% weight=75
     //% subcategory=Leds
@@ -253,8 +267,8 @@ namespace bitcommander
     }
 
     /**
-     * Shift LEDs forward and clear with zeros.
-     */
+      * Shift LEDs forward and clear with zeros.
+      */
     //% blockId="bitcommander_neo_shift" block="shift LEDs"
     //% weight=70
     //% subcategory=Leds
@@ -267,12 +281,25 @@ namespace bitcommander
     // advanced blocks
 
     /**
-     * Set the brightness of the LEDs
-     * @param brightness a measure of LED brightness (0 to 255) eg: 40
-     */
-    //% blockId="bitcommander_neo_brightness" block="set led brightness %brightness"
+      * Set LED update mode (Manual or Automatic)
+      * @param updateMode setting automatic will show LED changes automatically
+      */
+    //% blockId="bitcommander_set_updateMode" block="set %updateMode|update mode"
     //% brightness.min=0 brightness.max=255
     //% weight=65
+    //% advanced=true
+    export function neoBrightness(updateMode: BCMode): void
+    {
+        _updateMode = updateMode;
+    }
+
+    /**
+      * Set the brightness of the LEDs
+      * @param brightness a measure of LED brightness (0 to 255) eg: 40
+      */
+    //% blockId="bitcommander_neo_brightness" block="set led brightness %brightness"
+    //% brightness.min=0 brightness.max=255
+    //% weight=60
     //% advanced=true
     export function neoBrightness(brightness: number): void
     {
@@ -286,27 +313,28 @@ namespace bitcommander
       */
     //% advanced=true
     //% blockId="bc_colours" block=%color
-    //% weight=60
+    //% weight=55
     export function BCColours(color: BCColors): number
     {
         return color;
     }
 
     /**
-     * Convert from RGB values to colour number
-     *
-     * @param red Red value of the LED 0:255
-     * @param green Green value of the LED 0:255
-     * @param blue Blue value of the LED 0:255
-     */
+      * Convert from RGB values to colour number
+      *
+      * @param red Red value of the LED 0:255
+      * @param green Green value of the LED 0:255
+      * @param blue Blue value of the LED 0:255
+      */
     //% advanced=true
     //% blockId="bitcommander_convertRGB" block="convert from red %red| green %green| blue %bblue"
-    //% weight=55
+    //% weight=50
     //% advanced=true
     export function convertRGB(r: number, g: number, b: number): number
     {
         return ((r & 0xFF) << 16) | ((g & 0xFF) << 8) | (b & 0xFF);
     }
 
-
+    /**
+      
 }
