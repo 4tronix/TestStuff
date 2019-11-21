e2cc61813b076cee9494b39c1ed007adddf26e18
4tronix <Sales@4tronix.co.uk>
2019-11-21 10:22:06 +0000

    rover09

--- main.ts
+++ main.ts
@@ -208,7 +208,7 @@ namespace Rover
       * Initialise all servos to Angle=0
       */
     //% blockId="zeroServos"
-    //% block="Centre all 08 servos"
+    //% block="Centre all 09 servos"
     //% weight=100
     //% subcategory=Servos
     export function zeroServos(): void
@@ -228,12 +228,13 @@ namespace Rover
     //% subcategory=Servos
     export function steer(direction: eDirection, angle: number): void
     { 
+        angle = Math.max(Math.min(90, angle),0);
         if (direction==eDirection.Left)
             angle = 0-angle;
         setServo(getServoNumber(eServos.FL), angle);
         setServo(getServoNumber(eServos.FR), angle);
-        setServo(getServoNumber(eServos.RL), angle);
-        setServo(getServoNumber(eServos.RR), angle);
+        setServo(getServoNumber(eServos.RL), 0-angle);
+        setServo(getServoNumber(eServos.RR), 0-angle);
     }
 
     /**
