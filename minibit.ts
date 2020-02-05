bfc3e54a39ea98ffcfe3fcc0ae9b7056dd3e18ed
4tronix <Sales@4tronix.co.uk>
2020-02-05 15:15:51 +0000

    111

--- minibit.ts
+++ minibit.ts
@@ -260,7 +260,7 @@ namespace minibit
       * @param enable enable or disable Blueetoth
     */
     //% blockId="mbEnableBluetooth"
-    //% block="%enable| 110 Bluetooth"
+    //% block="%enable| 111 Bluetooth"
     //% blockGap=8
     export function mbEnableBluetooth(enable: mbBluetooth)
     {
