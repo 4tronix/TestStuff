b1e1f7726fd533620ad0e8241ec1f2a21a8cec13
4tronix <Sales@4tronix.co.uk>
2020-02-07 12:42:45 +0000

    130

--- minibit.ts
+++ minibit.ts
@@ -260,7 +260,7 @@ namespace minibit
       * @param enable enable or disable Blueetoth
     */
     //% blockId="mbEnableBluetooth"
-    //% block="%enable| 129 Bluetooth"
+    //% block="%enable| 130 Bluetooth"
     //% blockGap=8
     export function mbEnableBluetooth(enable: mbBluetooth)
     {
@@ -1441,7 +1441,7 @@ namespace minibit
     //% weight=30
     //% inlineInputMode=inline
     //% blockGap=8
-    export function oledCircle (x0: number, y0: number, r: number, doSet: boolen, update: boolean)
+    export function oledCircle (x0: number, y0: number, r: number, doSet: boolean, update: boolean)
     {
         let d3 = r / Math.sqrt(2);
         let x = 0;
