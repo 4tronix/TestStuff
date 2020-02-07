34e386b93d682caacbd7bda981e40cb6ae097a0c
4tronix <Sales@4tronix.co.uk>
2020-02-07 14:23:07 +0000

    131

--- minibit.ts
+++ minibit.ts
@@ -260,7 +260,7 @@ namespace minibit
       * @param enable enable or disable Blueetoth
     */
     //% blockId="mbEnableBluetooth"
-    //% block="%enable| 130 Bluetooth"
+    //% block="%enable| 131 Bluetooth"
     //% blockGap=8
     export function mbEnableBluetooth(enable: mbBluetooth)
     {
@@ -1354,7 +1354,7 @@ namespace minibit
     }
 
     /**
-      * draw a general line in any direction
+      * draw a line from x1,y1 to x2,y2
       * @param x1 x start
       * @param y1 y start
       * @param x2 x end
@@ -1362,7 +1362,7 @@ namespace minibit
       * @param doSet set or clear. eg: true
       * @param update set true to show immediately on screen. requires updateOled otherwise. eg: true
       */
-    //% blockId="OledGLine" block="OLED line from x,y%x1|,%y1|to x,y%x2|,%y2|set%doSet|update%update"
+    //% blockId="OledLine" block="OLED line from x,y%x1|,%y1|to x,y%x2|,%y2|set%doSet|update%update"
     //% inlineInputMode=inline
     //% doSet.shadow="toggleOnOff"
     //% update.shadow="toggleYesNo"
@@ -1371,34 +1371,9 @@ namespace minibit
     //% weight=45
     //% inlineInputMode=inline
     //% blockGap=8
-    export function oledGLine(x1: number, y1: number, x2: number, y2: number, doSet: boolean, update: boolean)
+    export function oledLine(x1: number, y1: number, x2: number, y2: number, doSet: boolean, update: boolean)
     {
-        let xSteps = Math.abs(x2-x1);
-        let ySteps = Math.abs(y2-y1);
-        let x = x1;
-        let y = y1;
-        if (xSteps > ySteps)    // x is greater, so step in x
-        {
-            let dx = (x2 > x1) ? 1 : -1;
-            let dy = (y2 - y1) / xSteps;
-            for (let i=0; i < xSteps; i++)
-            {
-                oScreen().plotPixel(x, Math.round(y), doSet, update);
-                x += dx;
-                y += dy;
-            }
-        }
-        else
-        {
-            let dx = (x2 - x1) / ySteps;
-            let dy = (y2 > y1) ? 1 : -1;
-            for (let i=0; i < ySteps; i++)
-            {
-                oScreen().plotPixel(Math.round(x), y, doSet, update);
-                x += dx;
-                y += dy;
-            }
-        }
+        oScreen().oledLine(x1, y1, x2, y2, doSet, update);
     }
 
     /**
@@ -1426,13 +1401,13 @@ namespace minibit
 
     /**
       * draw a circle
-      * @param x0 x centre
-      * @param y0 y centre
-      * @param r radius. eg: 30
+      * @param x x centre. eg: 60
+      * @param y y centre. eg: 30
+      * @param r radius. eg: 20
       * @param doSet set or clear. eg: true
       * @param update set true to show immediately on screen. Requires updateOled otherwise. eg: true
       */
-    //% blockId="OledCircle" block="OLED circle at x,y%x0|,%y0|radius%r|set%doSet|update%update"
+    //% blockId="OledCircle" block="OLED circle at x,y%x|,%y|radius%r|set%doSet|update%update"
     //% inlineInputMode=inline
     //% doSet.shadow="toggleOnOff"
     //% update.shadow="toggleYesNo"
@@ -1441,27 +1416,9 @@ namespace minibit
     //% weight=30
     //% inlineInputMode=inline
     //% blockGap=8
-    export function oledCircle (x0: number, y0: number, r: number, doSet: boolean, update: boolean)
+    export function oledCircle (x: number, y: number, r: number, doSet: boolean, update: boolean)
     {
-        let d3 = r / Math.sqrt(2);
-        let x = 0;
-        let y = 0;
-        while (x < d3)
-        {
-            y = Math.sqrt(r * r - x * x);
-            y = Math.round(y);
-            oScreen().plotPixel(x0 + x, y0 + y, doSet, false);
-            oScreen().plotPixel(x0 - x, y0 + y, doSet, false);
-            oScreen().plotPixel(x0 + x, y0 - y, doSet, false);
-            oScreen().plotPixel(x0 - x, y0 - y, doSet, false);
-            oScreen().plotPixel(x0 + y, y0 + x, doSet, false);
-            oScreen().plotPixel(x0 - y, y0 + x, doSet, false);
-            oScreen().plotPixel(x0 + y, y0 - x, doSet, false);
-            oScreen().plotPixel(x0 - y, y0 - x, doSet, false);
-            x += 1;
-        }
-        if (update)
-            oScreen().updateScreen();
+        oScreen().oledCircle(x, y, r, doSet, update);
     }
 
 }
