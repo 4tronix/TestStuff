018888b828faf0db8080209a06cd91af50930013
4tronix <Sales@4tronix.co.uk>
2020-02-19 15:10:34 +0000

    19

--- orbit.ts
+++ orbit.ts
@@ -41,21 +41,6 @@ namespace orbit
         return Math.max(Math.min(max, value), min);
     }
 
-// Block to enable Bluetooth and disable FireLeds.
-    /**
-      * Enable/Disable Bluetooth support by disabling/enabling FireLeds
-      * @param enable enable or disable Blueetoth
-    */
-    //% blockId="EnableBluetooth"
-    //% block="enable 18 Bluetooth & disable FireLeds%enable"
-    //% enable.shadow="toggleYesNo"
-    //% weight=100
-    //% blockGap=8
-    export function enableBluetooth(enable: boolean)
-    {
-        btEnabled = enable;
-    }
-
 // Generic FireLed Blocks
 
     // create a FireLed band if not got one already. Default to brightness 40
@@ -81,9 +66,10 @@ namespace orbit
       * Sets all LEDs to a given color (range 0-255 for r, g, b).
       * @param rgb RGB color of the LED
       */
-    //% blockId="SetLedColor" block="set all LEDs to%rgb=FireColours"
+    //% blockId="SetLedColor" block="set 19 all LEDs to%rgb=FireColours"
     //% subcategory=Generic
-    //% weight=80
+    //% group=Basic
+    //% weight=100
     //% blockGap=8
     export function setLedColor(rgb: number)
     {
@@ -96,7 +82,8 @@ namespace orbit
       */
     //% blockId="LedClear" block="clear all LEDs"
     //% subcategory=Generic
-    //% weight=70
+    //% group=Basic
+    //% weight=90
     //% blockGap=8
     export function ledClear()
     {
@@ -104,6 +91,107 @@ namespace orbit
         updateLEDs();
     }
 
+// Advanced generic blocks
+
+    /**
+      * Enable/Disable Bluetooth support by disabling/enabling FireLeds
+      * @param enable enable or disable Blueetoth
+    */
+    //% blockId="EnableBluetooth"
+    //% block="enable Bluetooth & disable FireLeds%enable"
+    //% enable.shadow="toggleYesNo"
+    //% subcategory=Generic
+    //% group=Advanced
+    //% weight=100
+    //% blockGap=8
+    export function enableBluetooth(enable: boolean)
+    {
+        btEnabled = enable;
+    }
+
+    /**
+     * Set the brightness of the FireLed band
+     * @param brightness a measure of LED brightness in 0-255. eg: 40
+     */
+    //% blockId="LedBrightness" block="set LED brightness%brightness"
+    //% subcategory=Generic
+    //% group=Advanced
+    //% brightness.min=0 brightness.max=255
+    //% weight=90
+    //% blockGap=8
+    export function ledBrightness(brightness: number)
+    {
+        fire().setBrightness(brightness);
+        updateLEDs();
+    }
+
+    /**
+      * Set LED update mode (Manual or Automatic)
+      * @param mode setting automatic will show LED changes automatically
+      */
+    //% blockId="SetUpdateMode" block="set%mode|update mode"
+    //% subcategory=Generic
+    //% group=Advanced
+    //% weight=80
+    //% blockGap=8
+    export function setUpdateMode(mode: ledMode)
+    {
+        updateMode = mode;
+    }
+
+    /**
+      * Show LED changes
+      */
+    //% blockId="LedShow" block="show LED changes"
+    //% subcategory=Generic
+    //% group=Advanced
+    //% weight=70
+    //% blockGap=8
+    export function ledShow(): void
+    {
+        if (! btEnabled)
+            fire().updateBand();
+    }
+
+    /**
+      * Get numeric value of colour
+      * @param colour Standard RGB Led Colours eg: #ff0000
+      */
+    //% blockId="FireColours" block=%colour
+    //% subcategory=Generic
+    //% group=Advanced
+    //% blockHidden=false
+    //% weight=60
+    //% blockGap=8
+    //% shim=TD_ID colorSecondary="#e7660b"
+    //% colour.fieldEditor="colornumber"
+    //% colour.fieldOptions.decompileLiterals=true
+    //% colour.defl='#ff0000'
+    //% colour.fieldOptions.colours='["#FF0000","#659900","#18E600","#80FF00","#00FF00","#FF8000","#D82600","#B24C00","#00FFC0","#00FF80","#FFC000","#FF0080","#FF00FF","#B09EFF","#00FFFF","#FFFF00","#8000FF","#0080FF","#0000FF","#FFFFFF","#FF8080","#80FF80","#40C0FF","#999999","#000000"]'
+    //% colour.fieldOptions.columns=5
+    //% colour.fieldOptions.className='rgbColorPicker'
+    export function fireColours(colour: number): number
+    {
+        return colour;
+    }
+
+    /**
+      * Convert from RGB values to colour number
+      * @param red Red value of the LED (0 to 255)
+      * @param green Green value of the LED (0 to 255)
+      * @param blue Blue value of the LED (0 to 255)
+      */
+    //% blockId="ConvertRGB" block="convert from red%red|green%green|blue%blue"
+    //% subcategory=Generic
+    //% group=Advanced
+    //% weight=50
+    //% blockGap=8
+    export function convertRGB(r: number, g: number, b: number): number
+    {
+        return ((r & 0xFF) << 16) | ((g & 0xFF) << 8) | (b & 0xFF);
+    }
+
+// Treat LEDs as Single String
     /**
      * Set single LED to a given color (range 0-255 for r, g, b).
      * @param ledId position of the LED (0 to 11)
@@ -111,7 +199,7 @@ namespace orbit
      */
     //% blockId="SetPixelColor" block="set LED at%ledId|to%rgb=FireColours"
     //% subcategory=SingleString
-    //% weight=60
+    //% weight=100
     //% blockGap=8
     export function setPixelColor(ledId: number, rgb: number)
     {
@@ -124,7 +212,7 @@ namespace orbit
       */
     //% blockId="LedRainbow" block="set LED rainbow"
     //% subcategory=SingleString
-    //% weight=50
+    //% weight=90
     //% blockGap=8
     export function ledRainbow()
     {
@@ -137,7 +225,7 @@ namespace orbit
      */
     //% blockId="LedShift" block="shift LEDs"
     //% subcategory=SingleString
-    //% weight=40
+    //% weight=80
     //% blockGap=8
     export function ledShift()
     {
@@ -150,7 +238,7 @@ namespace orbit
      */
     //% blockId="LedRotate" block="rotate LEDs"
     //% subcategory=SingleString
-    //% weight=30
+    //% weight=70
     //% blockGap=8
     export function ledRotate()
     {
@@ -232,7 +320,7 @@ namespace orbit
      */
     //% blockId="RainbowLatLong" block="set Rainbow in%latilong|%direction"
     //% subcategory="Latitude Longitude"
-    //% weight=90
+    //% weight=80
     //% blockGap=8
     export function rainbowLatLong(latilong: LatLong, direction: MoveDirection)
     {
@@ -267,7 +355,7 @@ namespace orbit
      */
     //% blockId="RotateLatLong" block="rotate all LEDs in%latilong|%direction"
     //% subcategory="Latitude Longitude"
-    //% weight=80
+    //% weight=70
     //% blockGap=8
     export function rotateLatLong(latilong: LatLong, direction: MoveDirection)
     {
@@ -357,88 +445,5 @@ namespace orbit
     }
 
 
-// Advanced generic blocks
-
-    /**
-     * Set the brightness of the FireLed band
-     * @param brightness a measure of LED brightness in 0-255. eg: 40
-     */
-    //% blockId="LedBrightness" block="set LED brightness%brightness"
-    //% subcategory=Generic
-    //% advanced=true
-    //% brightness.min=0 brightness.max=255
-    //% weight=100
-    //% blockGap=8
-    export function ledBrightness(brightness: number)
-    {
-        fire().setBrightness(brightness);
-        updateLEDs();
-    }
-
-    /**
-      * Set LED update mode (Manual or Automatic)
-      * @param mode setting automatic will show LED changes automatically
-      */
-    //% blockId="SetUpdateMode" block="set%mode|update mode"
-    //% subcategory=Generic
-    //% advanced=true
-    //% weight=90
-    //% blockGap=8
-    export function setUpdateMode(mode: ledMode)
-    {
-        updateMode = mode;
-    }
-
-    /**
-      * Show LED changes
-      */
-    //% blockId="LedShow" block="show LED changes"
-    //% subcategory=Generic
-    //% advanced=true
-    //% weight=80
-    //% blockGap=8
-    export function ledShow(): void
-    {
-        if (! btEnabled)
-            fire().updateBand();
-    }
-
-    /**
-      * Get numeric value of colour
-      * @param colour Standard RGB Led Colours eg: #ff0000
-      */
-    //% blockId="FireColours" block=%colour
-    //% subcategory=Generic
-    //% advanced=true
-    //% blockHidden=false
-    //% weight=70
-    //% blockGap=8
-    //% shim=TD_ID colorSecondary="#e7660b"
-    //% colour.fieldEditor="colornumber"
-    //% colour.fieldOptions.decompileLiterals=true
-    //% colour.defl='#ff0000'
-    //% colour.fieldOptions.colours='["#FF0000","#659900","#18E600","#80FF00","#00FF00","#FF8000","#D82600","#B24C00","#00FFC0","#00FF80","#FFC000","#FF0080","#FF00FF","#B09EFF","#00FFFF","#FFFF00","#8000FF","#0080FF","#0000FF","#FFFFFF","#FF8080","#80FF80","#40C0FF","#999999","#000000"]'
-    //% colour.fieldOptions.columns=5
-    //% colour.fieldOptions.className='rgbColorPicker'
-    export function fireColours(colour: number): number
-    {
-        return colour;
-    }
-
-    /**
-      * Convert from RGB values to colour number
-      * @param red Red value of the LED (0 to 255)
-      * @param green Green value of the LED (0 to 255)
-      * @param blue Blue value of the LED (0 to 255)
-      */
-    //% blockId="ConvertRGB" block="convert from red%red|green%green|blue%blue"
-    //% subcategory=Generic
-    //% advanced=true
-    //% weight=60
-    //% blockGap=8
-    export function convertRGB(r: number, g: number, b: number): number
-    {
-        return ((r & 0xFF) << 16) | ((g & 0xFF) << 8) | (b & 0xFF);
-    }
 
 }
