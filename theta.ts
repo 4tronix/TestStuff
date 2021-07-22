f813d7176e5eac26e0a64ce23b4ffe7ac38f77b1
4tronix <Sales@4tronix.co.uk>
2021-07-22 15:30:42 +0100

    th07

--- theta.ts
+++ theta.ts
@@ -509,7 +509,7 @@ namespace theta
       * @param direction select forwards or reverse
       * @param speed speed of motor between 0 and 100. eg: 60
       */
-    //% blockId="MotorMove" block="move 06 %motor|motor(s)%direction|at speed%speed|\\%"
+    //% blockId="MotorMove" block="move 07 %motor|motor(s)%direction|at speed%speed|\\%"
     //% weight=50
     //% speed.min=0 speed.max=100
     //% subcategory=Motors
@@ -549,7 +549,7 @@ namespace theta
         if (doPause)
         {
             robotStop(RXStopMode.Brake);
-            basic.pause(500);
+            basic.pause(100);
         }
 
         if ((motor == RXMotor.Left) || (motor == RXMotor.Both))
